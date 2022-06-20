35cfa49912f62147256c936bad8a113216eb1868
MathTH <matdhferreira@gmail.com>
2022-06-20 14:45:03 -0300

    Alteração

--- Teste_1/Funcionarios.sql
+++ Teste_1/Funcionarios.sql
@@ -27,3 +27,15 @@ INSERT INTO Funcionarios VALUES ( 'Matheus', 'Ferreira', '19', 'Jovem Aprendiz',
 INSERT INTO Funcionarios VALUES ('Emerson', 'Oliveira', '20', 'Estagiario', '2002-01-23', 'emerson.oliveira@gmail.com');
 INSERT INTO Funcionarios VALUES ('Angelo', 'Lempek', '23', 'Estagiario', '1999-08-12', 'angelo.lempek@gmail.com');
 INSERT INTO Funcionarios VALUES ('Kenji','Suzuki', '24', 'Estagiario','1998-04-15', 'kenji.suzuki@gmail.com');
+
+-- Puxando os dados de ID, NOME E CARGO / Criando uma nova tabela para colocar esses dados puxados da tabela funcionarios--
+SELECT ID, Nome, Cargo  INTO ##TESTE2  FROM Funcionarios ;
+
+
+SELECT  n.Nome, e.Email  
+FROM
+dbo.Funcionarios as n
+INNER JOIN dbo.Email e on n.id = e.id 
+
+
+
